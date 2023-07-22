7318764ac0c512d0535844fb670967ce78386f95
Dima Bart <3088742@mail.ru>
2023-07-23 00:13:13 +0300

    add header component

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import img from '../assets/page-not-found.png'
+import { Link } from 'react-router-dom';
+
 
 export const ErrorPage: React.FC = () => {
 	return (
-		<div>ErrorPage</div>
-	)
+    <div className="min-h-screen bg-slate-900 font-roboto text-white flex justify-center items-center flex-col gap-10">
+      <img src={img} alt="img" className='w-80'/>
+			<Link to={'/'} className='bg-sky-500 rounded-md px-6 py-2 hover:bg-sky-600' >
+				Back
+			</Link>
+    </div>
+  );
 }
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { Header } from '../components/Header'
 
 export const Layout: React.FC = () => {
 	return (
 		<div className='min-h-screen bg-slate-900 font-roboto text-white pb-20'>
-			<div>HEADER</div>
+			<Header/>
 			<div className='container'>
 				<Outlet/>
 			</div>
